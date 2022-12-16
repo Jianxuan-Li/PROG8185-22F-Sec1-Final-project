b099e732b30ceb64abde1300a9d8dffb7516b66e
Nikkon <nikkon@Nikkons-MacBook-Pro.local>
2022-12-16 12:30:01 -0500

    updated api urls + footer design

--- frontend/src/constants/apis.js
+++ frontend/src/constants/apis.js
@@ -1,6 +1,6 @@
-export const API_PRODUCT = API_HOST + API_PREFIX + "/product";
-export const API_COMMENT = API_HOST + API_PREFIX + "/comment/product";
-export const API_MEMBER = API_HOST + API_PREFIX + "/user";
-export const API_LOGIN = API_HOST + API_PREFIX + "/user/login";
-export const API_CART = API_HOST + API_PREFIX + "/cart";
-export const API_ORDER = API_HOST + API_PREFIX + "/order";
+export const API_PRODUCT = `${API_HOST + API_PREFIX}/product`;
+export const API_COMMENT = `${API_HOST + API_PREFIX}/comment/product`;
+export const API_MEMBER = `${API_HOST + API_PREFIX}/user`;
+export const API_LOGIN = `${API_HOST + API_PREFIX}/user/login`;
+export const API_CART = `${API_HOST + API_PREFIX}/cart`;
+export const API_ORDER = `${API_HOST + API_PREFIX}/order`;
